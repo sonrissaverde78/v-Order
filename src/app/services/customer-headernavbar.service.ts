142b392d99c6caeaef504fed1943f2e9e05f910c
anonimo <anonimo@anonimo.com>
2020-06-07 14:34:51 +0000

    modified wrapper in style.css, added customer products menu in customer-headernavbar.service.ts

--- src/app/services/customer-headernavbar.service.ts
+++ src/app/services/customer-headernavbar.service.ts
@@ -5,5 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class CustomerHeadernavbarService {
 
+  customerProductsMenu: any = [
+    {
+      titulo: 'Restaurant Test',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Starters', url: '/starters'},
+        { titulo: 'First Courses', url: '/firsts'},
+        { titulo: 'SecondCourses', url: '/seconds'},
+        { titulo: 'Desserts', url: '/desserts'},
+        { titulo: 'Drinks', url: 'drinks'}
+      ]
+    }
+  ];
   constructor() { }
 }
