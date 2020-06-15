74d9730f813fd6ac35ab145ff3f4fc428f7c2563
anonimo <anonimo@anonimo.com>
2020-06-15 13:22:13 +0000

    as the last comment

---
+++ src/app/components/product-card/product-card/product-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styles: [
+  ]
+})
+export class ProductCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
